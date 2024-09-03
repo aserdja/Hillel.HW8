fa27016fb28377108807819b7d481cfb498d348b
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-09-03 19:10:46 +0300

    Created thread generator and semaphore with 2 licenses

--- HW8/SemaphorePracticeApp/Program.cs
+++ HW8/SemaphorePracticeApp/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿var threadArray = new Thread[5];
+var semaphore = new Semaphore(2, 2);
+
+for (int i = 0; i < threadArray.Length; i++)
+{
+	threadArray[i] = new Thread(() =>
+	{
+		semaphore.WaitOne();
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} started");
+
+		var r = new Random();
+		Thread.Sleep(r.Next(1000, 5000));
+
+		Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} finished");
+		semaphore.Release();
+	});
+}
+
+foreach (var thread in threadArray)
+{
+	thread.Start();
+}
+
+foreach (var thread in threadArray)
+{
+	thread.Join();
+}
