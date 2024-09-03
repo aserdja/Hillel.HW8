95c6c89e5b4718cb557c4fe90ad667912d890904
Andrii Serdiuk <andreyserdjuk1@gmail.com>
2024-09-03 18:35:52 +0300

    Created ThreadPool and work items

--- HW8/ThreadPoolApp/Program.cs
+++ HW8/ThreadPoolApp/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using ThreadPoolApp;
+
+var workItems = new string[10]
+{
+	"Task 1",
+	"Task 2",
+	"Task 3",
+	"Task 4",
+	"Task 5",
+	"Task 6",
+	"Task 7",
+	"Task 8",
+	"Task 9",
+	"Task 10"
+};
+
+foreach (var item in workItems)
+{
+	ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadPoolInformator.ShowInfo), item);
+}
+
+Thread.Sleep(2000);
+
+
